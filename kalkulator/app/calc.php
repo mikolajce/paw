9e91616028c1365275006fee7610c212af99284c
Mikołaj Cieśliczka <mikolajce@gmail.com>
2022-03-14 20:43:20 +0100

    140322-20:43

---
+++ kalkulator/app/calc.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once dirname(__FILE__).'/../config.php';
+
+include _ROOT_PATH.'/app/security/check.php';
+
+// 1. pobranie parametrów
+
+function getParams(&$x,&$y,&$z){
+	$x = isset($_REQUEST['x']) ? $_REQUEST['x'] : null;
+	$y = isset($_REQUEST['y']) ? $_REQUEST['y'] : null;
+	$z = isset($_REQUEST['z']) ? $_REQUEST['z'] : null;
+}
+
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+
+function validate(&$x,&$y,&$z,&$messages){
+
+if ( ! (isset($x) && isset($y)  && isset($z))) {
+	return false;
+
+}
+
+// sprawdzenie, czy wartosci zostały podane
+if ( $x == "") {
+	$messages [] = 'Nie podano kwoty';
+}
+if ( $y == "") {
+	$messages [] = 'Nie podano lat kredytu';
+}
+if ( $z == "") {
+	$messages [] = 'Nie podano oprocentowania';
+}
+
+//nie ma sensu walidować dalej gdy brak parametrów
+if (count( $messages ) != 0) return false;
+
+	// sprawdzenie, czy $x i $y są liczbami całkowitymi
+    if (! is_numeric( $x )||!$x>0) {
+		     $messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+	}
+
+    if (! is_numeric( $y )||!$y>0) {
+		     $messages [] = 'Druga wartość nie jest liczbą większą od zera';
+	}
+    if (! is_numeric( $z )||!$z>0) {
+		     $messages [] = 'Trzecia wartość nie jest liczbą całkowitą';
+	}
+		if (count($messages) != 0) return false;
+		else return true;
+}
+
+// 3. Wykonanie operacji
+
+function process($x,$y,$z,&$messages,&$result){
+global $role;
+	//konwersja parametrów na int
+	      $x = intval($x);
+	      $y = intval($y);
+	      $z = intval($z);
+
+
+	//wykonanie operacji
+	if ($role != 'admin' &&$x>2000){
+		$messages[] = 'Tylko admin moze brac kredyt na wiecej niz 2000';
+}
+ else{
+	$z=12*$z;
+	$y=$y/100;
+	$result = ($x*$y)/(12*(1-((12/(12+$y))**$z)));
+}
+}
+
+$messages = array();
+getParams($x,$y,$z);
+	if ( validate($x,$y,$z,$messages) ) {
+ process($x,$y,$z,$messages,$result);
+
+}
+
+// 4. Wywołanie widoku z przekazaniem zmiennych
+
+include 'calc_view.php';
+
+?>
---
+++ kalkulator/app/calc_view.php
@@ -0,0 +1,64 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+
+<head>
+
+	<meta charset="utf-8" />
+
+	<title>Kalkulator</title>
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+
+</head>
+
+
+
+<body>
+
+	<div style="width:90%; margin: 2em auto;">
+	 <a href="<?php print(_APP_ROOT); ?>/app/security/logout.php" class="pure-button pure-button-active">Wyloguj</a>
+ 	</div>
+
+	<div style="width:90%; margin: 2em auto;">
+
+		<form action="<?php print(_APP_URL);?>/app/calc.php" method="post" class="pure-form pure-form-stacked" >
+			
+       <label for="id_x">Kwota kredytu: </label>
+       <input id="id_x" type="text" name="x" value="<?php out($x)?>"/<><br/>
+
+
+       <label for="id_y">Ilosc lat: </label>
+       <input id="id_y" type="text" name="y" value="<?php out($y)?>"/<><br/>
+
+       <label for="id_z">Oprocentowanie roczne:   <laber/>
+       <input id="id_z" type="text" name="z" value="<?php out($z)?>"/<><br/>
+
+       <input type="submit" value="Oblicz" class="pure-button pure-button-primary"/>
+
+       </form>
+
+<?php
+
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+
+?>
+
+<?php if (isset($result)){ ?>
+
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+	<?php echo 'Miesieczna rata: '.$result; ?>
+</div>
+
+<?php } ?>
+
+</body>
+
+</html>
