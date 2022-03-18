ba06a9708a1baef3e731ad1a65518944c8077ee8
Mikołaj Cieśliczka <mikolajce@gmail.com>
2022-03-18 14:37:26 +0100

    180322-14:37

--- kalkulator/app/security/check.php
+++ kalkulator/app/security/check.php
@@ -3,9 +3,7 @@ require_once dirname(__FILE__).'/../../config.php';
 
 session_start();
 
-
 $role = isset($_SESSION['role']) ? $_SESSION['role'] : '';
-
 
 if ( empty($role) ){
 	include _ROOT_PATH.'/app/security/login.php';
--- kalkulator/app/security/login_view.php
+++ kalkulator/app/security/login_view.php
@@ -5,6 +5,7 @@
 	<title>Logowanie</title>
 	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
 </head>
+
 <body>
 
 <div style="width:90%; margin: 2em auto;">
